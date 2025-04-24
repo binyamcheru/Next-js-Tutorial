b3afebe955eecef20fc954b91996815b17ff901c
Bina <benjaminbinyam500@gmail.com>
2025-04-24 08:56:34 +0300

    Fonts - Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Inter, Inconsolata, Roboto } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] });
+const inconsolata = Inconsolata({subsets: ['latin']})
+
+// For non-variable fonts, specify weights
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inconsolata.className}>
         <Navbar />
         <main className="max-w-3xl mx-auto py-10">{children}</main>
       </body>
