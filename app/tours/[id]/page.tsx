bd694f02ddd6afc68f47efc0e17a23faad24b5e9
Bina <benjaminbinyam500@gmail.com>
2025-05-08 09:34:25 +0300

    nested routes

---
+++ app/tours/[id]/page.tsx
@@ -0,0 +1,9 @@
+const page = ({ params }: { params: { id: string } }) => {
+  console.log(params);
+  return (
+    <div>
+      <h1 className="text-4xl">ID: {params.id}</h1>
+    </div>
+  );
+};
+export default page;
