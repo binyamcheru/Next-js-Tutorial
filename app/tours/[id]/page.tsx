34f085679986a71a8c38dfbe3239c77e47cc0870
Bina <benjaminbinyam500@gmail.com>
2025-05-08 11:11:45 +0300

    images in next js

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,8 +1,32 @@
+import mapsImg from "@/images/maps.jpg";
+import Image from "next/image";
+const url = "https://www.course-api.com/images/tours/tour-1.jpeg";
+
 const page = ({ params }: { params: { id: string } }) => {
   console.log(params);
   return (
     <div>
       <h1 className="text-4xl">ID: {params.id}</h1>
+      <section>
+        {/* For local images it is not must to provide width and height props */}
+        <Image
+          src={mapsImg}
+          alt="maps"
+          width={200}
+          height={200}
+          className="w-64 h-64 object-cover rounded"
+        />
+        <h2>Local image</h2>
+        {/* For remote images it is must to provide width and height props and add nextjs config file */}
+        <Image
+          src={url}
+          alt="maps"
+          width={200}
+          height={200}
+          className="w-64 h-64 object-cover rounded"
+        />
+        <h2>Remote image</h2>
+      </section>
     </div>
   );
 };
--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const url = "https://www.course-api.com/react-tours-project";
@@ -21,8 +22,7 @@ const fetchTours = async () => {
 async function ToursPage() {
   const data = await fetchTours();
   return (
-    <section>
-      <h1 className="text-3xl mb-4">Tours</h1>
+    <section className="grid md:grid-cols-2 gap-8">
       {data.map((tour) => {
         return (
           <Link
@@ -30,7 +30,18 @@ async function ToursPage() {
             href={`/tours/${tour.id}`}
             className="hover:text-blue-500 block"
           >
-            {tour.name}
+            <div className="relative h-48 mb-2">
+              <Image
+                src={tour.image}
+                alt={tour.name}
+                fill
+                sizes="33vw"
+                // sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw'
+                priority
+                className="object-cover rounded"
+              />
+            </div>
+            <h2>{tour.name}</h2>
           </Link>
         );
       })}
